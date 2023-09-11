181753c7ea8430ebdcc9a31a7553990cb166984f
spencaa <campspencer2@gmail.com>
2023-09-11 11:50:14 -0500

    Car example

--- Class/ConstructorExample/ConstructorExample/Source.cpp
+++ Class/ConstructorExample/ConstructorExample/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+/*
 // create class
 class Restaurant {
 	// define private and public stuff
@@ -33,5 +33,30 @@ int main() {
 	favLunchPlace.setName("Outback");
 	favLunchPlace.setRating(6);
 	favLunchPlace.print();
+	return 0;
+}
+*/
+
+// constructor with "default" parameters
+class Car {
+private:
+	string model;
+	int maxSpeed;
+public:
+	Car(string car_model = "Generic", int car_maxSpeed = -1) {
+		model = car_model;
+		maxSpeed = car_maxSpeed;
+	}
+	void print() {
+		cout << "Car model: " << model << " -- Max Speed: " << maxSpeed << endl;
+	}
+};
+
+int main() {
+	// call constructor
+	Car car1("Mazda", 116);
+	Car car2;
+	car1.print();
+	car2.print();
 	return 0;
 }
